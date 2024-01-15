6f308eb9c240997ce8876ade9f9a005ed6e98ae1
vsLoong <742420210@qq.com>
2024-01-15 09:43:45 +0800

    channel bug fix

--- core/common/src/test/kotlin/com/vsloong/toolman/core/common/test/ChannelTest.kt
+++ core/common/src/test/kotlin/com/vsloong/toolman/core/common/test/ChannelTest.kt
@@ -5,28 +5,33 @@ import com.vsloong.toolman.core.common.usecase.VasDollyUseCase
 import com.vsloong.toolman.core.common.usecase.WalleUseCase
 import com.vsloong.toolman.core.common.utils.logger
 import kotlin.io.path.Path
+import kotlin.io.path.name
+import kotlin.io.path.nameWithoutExtension
 
 fun main() {
 
     val channelUseCase: IChannelUseCase = VasDollyUseCase(assetsManager = TestAssetsManager)
 //    val channelUseCase: IChannelUseCase = WalleUseCase(assetsManager = TestAssetsManager)
 
-    val apkDir = Path("E:\\AndroidToolMan\\apk\\")
-    val apkPath = apkDir.resolve("sample.apk")
+//    val apkDir = Path("E:\\AndroidToolMan\\apk\\sample.apk")
+    val apkPath = Path("/Users/dragon/Temp/HC/app-lite-qa-debug.apk")
+    val srcApkName = apkPath.nameWithoutExtension
+
 
     // 添加渠道信息
+    val channel = "xiaomi"
     channelUseCase.putChannel(
         apkPath = apkPath,
-        channels = setOf("xiaomi")
+        channels = setOf(channel)
     )
 
     // 获取渠道信息
-    val channelApkPath = apkDir.resolve("sample_xiaomi.apk")
-    val channel = channelUseCase.getChannel(
+    val channelApkPath = (apkPath.parent).resolve("${srcApkName}_${channel}.apk")
+    val channelInfo = channelUseCase.getChannel(
         apkPath = channelApkPath
     )
 
-    logger("获取到的渠道信息是：$channel")
+    logger("获取到的渠道信息是：$channelInfo")
 
     // 删除渠道信息
     channelUseCase.removeChannel(
